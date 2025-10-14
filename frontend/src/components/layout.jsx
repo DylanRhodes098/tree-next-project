e0125dbd2a4b6bca0ad4c318a2a8a512e935e3a7
dylanrhodes098 <dylanrhodes098@gmail.com>
2025-10-14 14:58:51 +0100

    added topnav

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
 import SideNav from "./sideNav";
+import TopNav from "./topNav";
 
 export default function Layout () {
     
     return (
         <>
-        
         <div>
+        <TopNav></TopNav>
       <SideNav></SideNav>
       <Outlet />
     </div>
-     
         </>
     )
 }
---
+++ frontend/src/pages/userProfile.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+import {userId} from "../services/user";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function UserProfile () {
+    return (
+        <>
+        <h1>userProfile</h1>
+        </>
+    )
+}
